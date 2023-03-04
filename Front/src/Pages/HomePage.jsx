a1a9646c521d95a1e4dde9822662ed503f3d3928
Enkhbilguuuun <110895002+Enkhbilguuuun@users.noreply.github.com>
2023-03-04 17:37:41 +0800

    2 homepage big ads fe (#27)
    hope its done

--- Front/src/Pages/HomePage.jsx
+++ Front/src/Pages/HomePage.jsx
@@ -1,13 +1,26 @@
 import Menu from "../Components/Menu";
 import { createContext } from "react";
+import { useState } from "react";
 import Navbar from "../Components/Navbar";
+import Ads from "../Components/BigAds";
 export const MenuChange = createContext();
 
 const HomePage = () => {
   return (
     <div>
-      <Navbar />
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Ads/>
+      </div>
     </div>
+    // <MenuChange.Provider value={{ menu, setMenu }}>
+    //   <div className="App">
+    //     {menu ? <MenuClicked /> : <Menu />}
+    //     <div className="www"> </div>
+    //   </div>
+    // </MenuChange.Provider>
   );
 };
 export default HomePage;

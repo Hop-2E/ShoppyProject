3dd11470ed893bc01402778bc990dbf7b887b04f
Enkhbilguun <2022HopE_Enkhbilguun@Mstarslab402s-iMac.local>
2023-03-04 17:28:36 +0800

    test

---
+++ Front/src/Components/Coupons.jsx
@@ -0,0 +1,12 @@
+import shoppy1 from "../Assets/shoppy1.webp"
+
+
+function Coupons(){
+    return(
+        <div className="coupcont">
+            
+        </div>
+    )
+}
+
+export default Coupons
